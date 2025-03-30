fec4638b57897a17ede1e9400440cb4147162df5
najmeh <najmehkhani.95@gmail.com>
2025-03-30 11:15:50 -0700

    study grade prediction

--- LLS/Student_grade/LLS.py
+++ LLS/Student_grade/LLS.py
@@ -8,12 +8,13 @@ class LLS:
     def calculate_weights(self, X, y):
      
         
-        X_with_bias = np.c_[np.ones(X.shape[0]), X]
-        self.w = np.linalg.inv(X_with_bias.T * X_with_bias) * X_with_bias.T * y
+        #X_with_bias = np.c_[np.ones(X.shape[0]), X]
+        self.w = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
+        return self.w
 
     def predict(self, X):
     
         if self.w is None:
             raise ValueError("Weights have not been calculated. Please call 'calculate_weights' first.")
-        X_with_bias = np.c_[np.ones(X.shape[0]), X]
-        return X_with_bias * self.w
+        #X_with_bias = np.c_[np.ones(X.shape[0]), X]
+        return X * self.w
